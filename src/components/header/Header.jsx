bf338c050b0fed2b36331e45bf3054407b668b88
Mark Vargas <mav49er@gmail.com>
2023-12-12 21:18:15 -0600

    Updating header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,16 +1,22 @@
 import React from "react";
+import Form from "react-bootstrap/Form";
+import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
+import Button from "react-bootstrap/Button";
 
 function Header() {
   return (
-    <Navbar bg="primary" data-bs-theme="dark">
-      <Navbar.Brand href="#home" style={{ marginLeft: "1%" }}>
-        Weather App
-      </Navbar.Brand>
-      <Navbar.Toggle />
-      <Navbar.Collapse className="justify-content-end">
-        <Navbar.Text style={{ marginRight: "1%" }}>By Mark Vargas</Navbar.Text>
-      </Navbar.Collapse>
+    <Navbar expand="lg" bg="primary" data-bs-theme="light">
+      <Container fluid>
+        <Navbar.Brand style={{ color: "#ffffff" }}>Weather App</Navbar.Brand>
+        <Form className="d-flex">
+          <Form.Control type="search" className="me-2" placeholder="Search" />
+          <Button variant="primary" style={{ borderColor: "#000000" }}>
+            Search
+          </Button>
+        </Form>
+        <Navbar.Text style={{ color: "#ffffff" }}>By Mark Vargas</Navbar.Text>
+      </Container>
     </Navbar>
   );
 }
